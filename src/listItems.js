e782bf8153315c847d6f2a01fb17f1ada97a236a
Julie Pitman <juliepitman@Julies-MBP.lan>
2021-03-15 14:23:29 -0700

    on press features

--- src/listItems.js
+++ src/listItems.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Pressable } from 'react-native';
 
 
 
 const ListItems = ({items}) => {
     return(
-        items.map((item) => (
-        <View style={styles.listItem}>
-            <Text>{item}</Text>
-        </View> 
+        items.map((item,i) => (
+            <Pressable
+            onPress={() => alert('fast press occurred')}
+            onLongPress={() => alert('long press occurred')}
+            onPressIn={() => alert('pressing button in')}
+            onPressOut={() => alert('releasing button press')}
+
+            >
+                <View 
+                key={item}
+                style={styles.listItem}>
+                    <Text>{item}</Text>
+                </View> 
+        </Pressable>
     ))
     )
 }
